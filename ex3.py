74be55c2d91b669b51067877401ea0578bc87667
sameera p <sameera.pajjuru119@gmail.com>
2020-04-29 13:23:32 +0530

    solved ex3

--- ex3.py
+++ ex3.py
@@ -1,3 +1,10 @@
-num=int(input("enter the number"))
-a=filter(lambda x:(x<num),[1,2,4,5,7,9,12,17,26,45,67,76])
+num=int(input("enter the number"))#a=filter(lambda x:(x<num),[1,2,4,5,7,9,12,17,26,45,67,76])
 print(list(a))
+num=int(input("enter the number"))
+#ANOTHER MODEL
+a=[1,2,3,4,5,6,7,8,9]
+new=[]
+for i in a:
+    if i<num:
+        new.append(i)
+print("less than number",list(new))
