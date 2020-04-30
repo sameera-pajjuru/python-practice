068729c8595c0a0f706947462666ca263aae0ccd
sameera-pajjuru <64326849+sameera-pajjuru@users.noreply.github.com>
2020-04-30 11:22:58 +0530

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,7 +1,8 @@
-num=int(input("enter the number"))#a=filter(lambda x:(x<num),[1,2,4,5,7,9,12,17,26,45,67,76])
-print(list(a))
 num=int(input("enter the number"))
+a=filter(lambda x:(x<num),[1,2,4,5,7,9,12,17,26,45,67,76])
+print(list(a))
 #ANOTHER MODEL
+num=int(input("enter the number"))
 a=[1,2,3,4,5,6,7,8,9]
 new=[]
 for i in a:
